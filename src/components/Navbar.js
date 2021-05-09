d25b7c05a276d38c2f47e74f3185975c8d052e1c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-09 16:59:33 -0400

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { AiOutlineGift } from "react-icons/ai";
@@ -10,7 +11,7 @@ export default function Navbar() {
         <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Netflix</a>
+          <a className="navbar-brand" href="#"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" className="netflixLogo" alt="Netflix Logo" /></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -47,7 +48,7 @@ export default function Navbar() {
                         <a className="nav-link" href="#"><BsFillBellFill size={24} /></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Logo</a>
+                        <a className="nav-link" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" className="profileImage" alt="Profile Image" /></a>
                     </li>
                 </ul>
             </div>
