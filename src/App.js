fb2a5a7535a375724bc336da8bafd30fa3537689
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-11 11:12:19 -0400

    Netflix Clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { API } from './components/API';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <API />
     </div>
   );
 }
