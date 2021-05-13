ebda64bf5e190c0f3fb045991e321cdeacb4c50c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-13 11:10:25 -0400

    buttons

--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -7,6 +7,10 @@ class MainMovie extends React.Component {
           <div className="imageContainer">
             <img src={this.props.movieBackdrop} alt="Movie or TV Show Back Drop Picture" className="mainMovie" />
             <div className="movieTitle"><h1>{this.props.movieTitle}</h1></div>
+            <div className="buttonMain">
+              <button className="btn btn-light playButton" type="button">Play</button>
+              <button className="btn btn-light moreInfoButton" type="button">More Info</button>
+            </div>
           </div>
         )
     }
