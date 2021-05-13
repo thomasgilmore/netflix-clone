10a4567f569299ecff2e6c8a3762b31a492b5cb0
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-13 11:40:30 -0400

    buttons

--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './mainmovie.css';
+import { GrPlayFill } from "react-icons/gr";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 class MainMovie extends React.Component {
     render() {
@@ -8,8 +10,8 @@ class MainMovie extends React.Component {
             <img src={this.props.movieBackdrop} alt="Movie or TV Show Back Drop" className="mainMovie" />
             <div className="movieTitle"><h1>{this.props.movieTitle}</h1></div>
             <div className="buttonMain">
-              <button className="btn btn-light playButton" type="button">Play</button>
-              <button className="btn btn-light moreInfoButton" type="button">More Info</button>
+              <button className="btn btn-light playButton" type="button"><GrPlayFill className="playIcon" /><span className="playText"> Play</span></button>
+              <button className="btn btn-light moreInfoButton" type="button"><AiOutlineInfoCircle className="infoIcon" /><span className="infoText"> More Info</span></button>
             </div>
           </div>
         )
