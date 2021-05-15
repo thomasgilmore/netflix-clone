21d9ae8e4614546d208ca199ad45c25edcba6bb3
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-14 20:56:59 -0400

    Mystery

--- src/components/API.js
+++ src/components/API.js
@@ -23,6 +23,7 @@ export class API extends Component {
         const urlString4 = "https://api.themoviedb.org/3/movie/18/similar?api_key=" + process.env.REACT_APP_MOVIE_DB_API + "&language=en-US&page=1";
         const urlString5 = "https://api.themoviedb.org/3/movie/12/similar?api_key=" + process.env.REACT_APP_MOVIE_DB_API + "&language=en-US&page=1";
         const urlString6 = "https://api.themoviedb.org/3/movie/10770/similar?api_key=" + process.env.REACT_APP_MOVIE_DB_API + "&language=en-US&page=1";
+        const urlString7 = "https://api.themoviedb.org/3/movie/9648/similar?api_key=" + process.env.REACT_APP_MOVIE_DB_API + "&language=en-US&page=1";
 
 
 
@@ -198,6 +199,32 @@ export class API extends Component {
             console.log("Failed to fetch data")
           }
         })
+
+        $.ajax({
+          url: urlString7,
+          success: (searchResults) => {
+            console.log("Fetched data successfully")
+            console.log(searchResults)
+            const results = searchResults.results
+            console.log(results)
+
+            var mysteries = []
+
+            results.forEach((movie) => {
+              var movieTitle = movie.name || movie.title;
+              var movieBackdrop = "https://image.tmdb.org/t/p/original" + movie.backdrop_path;
+              // var moviePoster = "https://image.tmdb.org/t/p/w440_and_h660_face" + movie.poster_path;
+              var movieId = movie.id;
+              const mystery = <MovieRow key={movieId} movieTitle={movieTitle} movieBackdrop={movieBackdrop} />
+              mysteries.push(mystery)
+            })
+     
+            this.setState({mysteries})
+          },
+          error: (xhr, status, err) => {
+            console.log("Failed to fetch data")
+          }
+        })
       }
     
     //   searchChangeHandler(event) {
@@ -240,6 +267,12 @@ export class API extends Component {
             <h3 className="trendingTitle">Dramas</h3>
             <div className="trendingMoviesAndTVShows">
               {this.state.dramas}
+            </div>
+          </section>
+          <section className="trendingSection">
+            <h3 className="trendingTitle">Mystery</h3>
+            <div className="trendingMoviesAndTVShows">
+              {this.state.mysteries}
             </div>
           </section>
           <section className="trendingSection">
